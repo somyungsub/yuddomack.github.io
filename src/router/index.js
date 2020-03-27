78ca20178aa8ec1ec16ddf92538d538057028c59
yuddomack <darr880406@gmail.com>
2020-03-27 14:27:34 +0900

    atomic pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
-import PostView from '../pages/PostView';
+import PostView from '@/components/pages/PostView';
 
 Vue.use(Router);
 
